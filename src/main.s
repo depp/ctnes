82857bf754311bf36a212f93d7ddcdb38483ee77
Dietrich Epp <depp@zdome.net>
2018-01-10 23:58:18 -0500

    draw sprites on screen

--- src/main.s
+++ src/main.s
@@ -12,12 +12,13 @@ PPUSCROLL	= $2005
 PPUADDR		= $2006
 PPUDATA		= $2007
 
+OAMDMA		= $4014
 APUSTATUS	= $4015
 
 .segment "INES"
 .byte $4e, $45, $53, $1a
 .byte 1		; Size of PRG in units of 16 KiB.
-.byte 0		; Size of CHR in units of 8 KiB.
+.byte 1		; Size of CHR in units of 8 KiB.
 .byte 1		; Flags, vertical mirroring.
 
 .code
@@ -52,16 +53,77 @@ reset:
 	inx
 	bne :-
 
+	; generate sprites
+	ldy #$0
+:
+	tya
+	asl
+	asl
+	tax
+
+	asl
+	and #$38
+	adc #$20
+	sta $0203, x	; X position
+
+	tya
+	and #$38
+	adc #$20
+	sta $0200, x	; Y position
+
+	tya
+	and #$03
+	sta $0202, x	; Palette
+
+	iny
+	tya
+	sta $0201, x	; Tile
+	cpy #$40
+
+	bne :-
+
+
 :	bit PPUSTATUS
 	bpl :-
 
-	lda #$80
+	lda #%10000000
+	sta PPUCTRL
+	lda #%00010000
 	sta PPUMASK
 forever:
 	jmp forever
 
+palette_data:
+	.byte $0f,$11,$21,$31,$0f,$14,$24,$34,$0f,$17,$27,$37,$0f,$1a,$2a,$3a
+	.byte $0f,$11,$21,$31,$0f,$14,$24,$34,$0f,$17,$27,$37,$0f,$1a,$2a,$3a
+
+object_data:
+	.byte $80,$01,$00,$80
+	.byte $80,$02,$01,$90
+	.byte $80,$03,$02,$a0
+	.byte $80,$04,$03,$b0
+
 ; NMI interrupt (vertical blank).
 nmi:
+	; load palette
+	lda PPUSTATUS
+	lda #$3f
+	sta PPUADDR
+	lda #$10
+	sta PPUADDR
+	ldx #0
+:	lda palette_data, x
+	sta PPUDATA
+	inx
+	cpx #$20
+	bne :-
+
+	; load sprites
+	lda #0
+	sta OAMADDR
+	lda #$02
+	sta OAMDMA
+
 	rti
 
 ; IRQ interrupt.
@@ -72,3 +134,6 @@ irq:
 .addr nmi
 .addr reset
 .addr irq
+
+.segment "CHR0"
+.incbin "../data/font.chr"
--- src/main.x
+++ src/main.x
@@ -1,6 +1,6 @@
 MEMORY {
     HEADER: start = $0000, size = $0010, type = rw, file = %O, fill = yes;
-    PRG0:   start = $8000, size = $4000, type = ro, file = %O, fill = yes; 
+    PRG0:   start = $8000, size = $4000, type = ro, file = %O, fill = yes;
     CHR0:   start = $0000, size = $2000, type = ro, file = %O, fill = yes;
 }
 
@@ -8,4 +8,5 @@ SEGMENTS {
     INES:   load = HEADER, type = ro, align = $10;
     CODE:   load = PRG0, type = ro;
     VECTOR: load = PRG0, type = ro, start = $BFFA;
+    CHR0:   load = CHR0, type = ro;
 }
